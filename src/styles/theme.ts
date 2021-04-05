c442bfc57ec5b49fcd7ec29bd7fc9b12346c61fb
Gisele Bernardes da Silva <giiselebernardes@gmail.com>
2021-04-05 08:30:28 -0300

    configurando chakra ui

---
+++ src/styles/theme.ts
@@ -0,0 +1,27 @@
+import { extendTheme } from "@chakra-ui/react";
+
+export const theme = extendTheme({
+  colors: {
+    gray: {
+      "500": "#47585B",
+      "400": "#999999",
+      "100": "#DADADA",
+      "50": "#F5F8FA",
+    },
+    yellow: {
+      "500": "FFBA08",
+    },
+  },
+  fonts: {
+    heading: "Poppins",
+    body: "Poppins",
+  },
+  styles: {
+    global: {
+      body: {
+        bg: "gray.50",
+        color: "gray.500",
+      },
+    },
+  },
+});
